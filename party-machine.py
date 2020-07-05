46c5fbbeec9c7a46c47e3bdcabf40ad7ff702320
Gamer2020 <lostatlife2020@hotmail.com>
2020-07-04 21:52:45 -0400

    Base enemy detection on name

--- party-machine.py
+++ party-machine.py
@@ -12,16 +12,21 @@ import serial
 import yaml
 import time
 
+from PIL import Image
+
+import pytesseract
+
 config_file = "config.yaml"
 
 
 def check_for_enemy():
-    marker_location = pyscreeze.locateCenterOnScreen(
-        "img/enemy/enemy_top.png", grayscale=True
-    )
+    # marker_location = pyscreeze.locateCenterOnScreen(
+    #     "img/enemy/enemy_top.png", grayscale=True
+    # )
+    enemy_name = get_enemy_name()
 
-    if marker_location == None:
-        print("Enemy not detected!")
+    if enemy_name == "":
+        # print("Enemy not detected!")
         return False
     else:
         return True
@@ -30,6 +35,9 @@ def check_for_enemy():
 def get_enemy_name():
     enemyScreen = ImageGrab.grab(bbox=(630, 30, 890, 48))
     enemyScreen.save("img_out/enemy_name.png")
+    im = Image.open("img_out/enemy_name.png")
+    # im = Image.open(enemyScreen)
+    return pytesseract.image_to_string(im)
 
 
 if os.path.exists(config_file) is False:
@@ -59,7 +67,6 @@ time.sleep(2)
 while 1:
     arduinoSerial.write(b"q")
     time.sleep(1)
-    get_enemy_name()
 
     if check_for_enemy() == True:
         while check_for_enemy():
