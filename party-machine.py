1b4ae713038dcecf58fa7ec702cf0133a790b9f4
Gamer2020 <lostatlife2020@hotmail.com>
2020-07-04 21:43:21 -0400

    Start trying to get name

--- party-machine.py
+++ party-machine.py
@@ -17,10 +17,11 @@ config_file = "config.yaml"
 
 def check_for_enemy():
     marker_location = pyscreeze.locateCenterOnScreen(
-        "img/enemy/enemy_marker.png", grayscale=True
+        "img/enemy/enemy_top.png", grayscale=True
     )
 
     if marker_location == None:
+        print("Enemy not detected!")
         return False
     else:
         return True
@@ -29,7 +30,6 @@ def check_for_enemy():
 def get_enemy_name():
     enemyScreen = ImageGrab.grab(bbox=(630, 30, 890, 48))
     enemyScreen.save("img_out/enemy_name.png")
-    quit()
 
 
 if os.path.exists(config_file) is False:
@@ -59,6 +59,7 @@ time.sleep(2)
 while 1:
     arduinoSerial.write(b"q")
     time.sleep(1)
+    get_enemy_name()
 
     if check_for_enemy() == True:
         while check_for_enemy():
